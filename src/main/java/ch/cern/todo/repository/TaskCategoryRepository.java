9db047a7cc5d679ebeca3a00c7b6201e2a181275
Agata Kaczmarek <56119853+kaczmareka@users.noreply.github.com>
2023-01-12 11:27:57 +0100

    delete unnecessary comments

--- src/main/java/ch/cern/todo/repository/TaskCategoryRepository.java
+++ src/main/java/ch/cern/todo/repository/TaskCategoryRepository.java
@@ -1,8 +1,6 @@
 package ch.cern.todo.repository;
 
-//import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import ch.cern.todo.model.TaskCategories;
 //extends jpa repository for CRUD methods
 public interface TaskCategoryRepository extends JpaRepository<TaskCategories, Long>{
