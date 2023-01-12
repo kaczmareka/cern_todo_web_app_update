2509072105c82a2f4078846d2a35f66a581d9012
Agata Kaczmarek <56119853+kaczmareka@users.noreply.github.com>
2023-01-12 11:28:23 +0100

    delete unnecessary comments

--- src/main/java/ch/cern/todo/repository/TasksRepository.java
+++ src/main/java/ch/cern/todo/repository/TasksRepository.java
@@ -1,6 +1,5 @@
 package ch.cern.todo.repository;
 
-//import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ch.cern.todo.model.Tasks;
@@ -8,12 +7,3 @@ import ch.cern.todo.model.Tasks;
 public interface TasksRepository extends JpaRepository<Tasks, Long>{
 
 }
-
-//http://localhost:8080/api/tasks
-
-//{"task_name": "kaczka2",
-// "task_description": "jajko",
-// "deadline": "2023-01-12 12:00:00",
-// "category_name": "ciekawe",
-// "category_description": "bardzo ciekawe"
-//}
